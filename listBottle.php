208243f6b1b451c902da7595f83405c8ab44f0d6
Théo N <theo.notte@hotmail.fr>
2021-03-21 13:31:03 +0100

    update title and create listBottle

---
+++ listBottle.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+$server = 'localhost';
+$database = 'DrinkMobBdd';
+$user = 'user';
+$password = '123';
+$conn = @mysqli_connect($server,$user,$password,$database);
+if(!$conn){
+    echo"Erreur de connexion au serveur !";
+    echo $server;
+    echo $database;
+    echo $user;
+    exit;
+}
+
+
+
+
+$listBottle = array();
+$selectBottle = "SELECT * 
+                 FROM Bottle
+                 ";
+
+$reqBottle = $conn->query($selectBottle);
+while ($row = mysqli_fetch_row($reqBottle)) {
+  $list = array( 'id' =>  $row[0], 'name' => $row[1], 'amount' =>  $row[2], 'score' =>  $row[3], 'description' =>  $row[4], 'categorieID' => $row[5]);
+  array_push($listBottle,$list);
+}
+
+echo json_encode($listBottle);
+return json_encode($listBottle);
+
+?>
